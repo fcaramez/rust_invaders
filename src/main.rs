8dde8611c504cf441c4f112648f1d34287586202
fcaramez <francisco.caramez.duarte@gmail.com>
2023-10-25 02:55:59 +0100

    loads all audios

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rusty_audio::Audio;
+use std::{error::Error, fs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    for item in &["explode", "lose", "move", "pew", "startup", "win"] {
+        audio.add(item, &format!("sounds/{}.wav", item));
+    }
+    audio.play("startup");
+
+    audio.wait();
+
+    Ok(())
 }
