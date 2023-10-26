4e544f73148091e8914d6fc43d62a03c12dc168f
fcaramez <francisco.caramez.duarte@gmail.com>
2023-10-26 01:36:24 +0100

    adds collision, win and lose conditions. Game done :)

--- src/main.rs
+++ src/main.rs
@@ -91,8 +91,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // Updates
         player.update(delta);
+
         if invaders.update(delta) {
             audio.play("move");
+        }
+
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
         }
 
         // Draw and render
@@ -104,6 +109,18 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // Win or Lose
+
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gapeloop;
+        }
+
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gapeloop;
+        }
     }
 
     // Cleanup
